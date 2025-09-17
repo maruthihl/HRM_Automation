81a06e2a257e94b5b6b910b512d99d37acc98be9
maruthihl <maruthihl2000@gmail.com>
2025-09-17 18:35:56 +0530

    OrangeHRM Automation setup

---
+++ src/test/java/com/example/orangehrm/tests/LoginTest.java
@@ -0,0 +1,12 @@
+package com.example.orangehrm.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest
+{
+    @Test
+    public void testLogin()
+    {
+        System.out.println("Login test executed");
+    }
+}
